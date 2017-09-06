faeb229fff6ac65e8d34199a2df43974b9e123d7
Tollef Jørgensen <tollefj@gmail.com>
2017-09-06 10:28:53 +0200

    basic boilerplate

--- project2/app.js
+++ project2/app.js
@@ -1,10 +1,24 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+
+// include styles (.css files)
+app.use(express.static(__dirname + '/styles'));
+// include scripts (.js files)
+app.use(express.static(__dirname + '/script'));
 
 app.get('/', function (req, res) {
-  res.send('Dis is project 2')
-})
+  res.sendFile('index.html');
+});
+
+app.get('/somepage', function (req, res) {
+  res.sendFile('somepage.html');
+});
+
+app.get('/anotherpage', function (req, res) {
+  res.sendFile('anotherpage.html');
+});
 
 app.listen(3000, function () {
   console.log('listening on port 3000!')
 })
+
