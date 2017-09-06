553bc9eacc666c13e1ea2a47552084c070de3725
Tollef Jørgensen <tollefj@gmail.com>
2017-09-06 10:42:52 +0200

    fixed appjs dirs

--- project2/app.js
+++ project2/app.js
@@ -2,21 +2,21 @@ const express = require('express');
 const app = express();
 
 // include styles (.css files)
-app.use(express.static(__dirname + '/styles'));
+app.use(express.static(__dirname + '/Styles'));
 // include scripts (.js files)
-app.use(express.static(__dirname + '/script'));
+app.use(express.static(__dirname + '/Script'));
 
 app.get('/', function (req, res) {
   res.sendFile('index.html');
 });
 
-app.get('/somepage', function (req, res) {
-  res.sendFile('somepage.html');
-});
-
-app.get('/anotherpage', function (req, res) {
-  res.sendFile('anotherpage.html');
-});
+// app.get('/somepage', function (req, res) {
+//   res.sendFile('somepage.html');
+// });
+//
+// app.get('/anotherpage', function (req, res) {
+//   res.sendFile('anotherpage.html');
+// });
 
 app.listen(3000, function () {
   console.log('listening on port 3000!')
