ec6c3c98cd78a048d83605b0c16535514c19fac1
Tollef Jørgensen <tollefj@gmail.com>
2017-09-06 18:12:52 +0200

    working routes

--- project2/routes/index.js
+++ project2/routes/index.js
@@ -6,4 +6,14 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+function new_route(path){
+  router.get('/'+path, function(req, res, next) {
+    res.render(path, { title: 'Title'});
+  });
+}
+new_route('waffle');
+new_route('cake');
+new_route('cookie');
+new_route('candy');
+
 module.exports = router;
